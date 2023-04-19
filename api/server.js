06beeadfe9cbc2dbfa6d0f50e2a2899ba8149ee3
Melis Kovancı <meliskovanci@hotmail.com>
2023-04-19 09:10:02 +0300

    ödev tamamlandı

--- api/server.js
+++ api/server.js
@@ -1,3 +1,15 @@
 // server için gerekli olanları burada ayarlayın
 
 // posts router'ını buraya require edin ve bağlayın
+
+const express = require("express");
+
+const postsRouter = require("./posts/posts-router");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/posts", postsRouter);
+
+module.exports = server;
